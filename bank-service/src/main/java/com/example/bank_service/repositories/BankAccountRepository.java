c104ab120ffe5a03a04e341d52e616b8ca010a9c
nissrine-elf <elfijaoui.nissrine@gamil.com>
2025-10-11 21:14:43 +0100

    add projection p1

--- bank-service/src/main/java/com/example/bank_service/repositories/BankAccountRepository.java
+++ bank-service/src/main/java/com/example/bank_service/repositories/BankAccountRepository.java
@@ -1,9 +1,13 @@
 package com.example.bank_service.repositories;
 
 import com.example.bank_service.entities.BankAccount;
+import com.example.bank_service.enums.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 
+import java.util.List;
+
 @RepositoryRestController
 public interface BankAccountRepository extends JpaRepository<BankAccount,String> {
+    List<BankAccount> findByType(AccountType type);
 }
