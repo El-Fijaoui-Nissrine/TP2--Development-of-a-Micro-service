fa5ceded37ecb374f309f17e4c7e6fe5fc02e85c
nissrine-elf <elfijaoui.nissrine@gamil.com>
2025-10-15 20:59:36 +0100

    graphql first commit

---
+++ bank-service/src/main/java/com/example/bank_service/web/BankAccountGraphQlController.java
@@ -0,0 +1,36 @@
+package com.example.bank_service.web;
+
+import com.example.bank_service.dto.BankAccountRequestDTO;
+import com.example.bank_service.dto.BankAccountResponseDTO;
+import com.example.bank_service.entities.BankAccount;
+import com.example.bank_service.repositories.BankAccountRepository;
+import com.example.bank_service.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+@Controller
+public class BankAccountGraphQlController {
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
+    @Autowired
+    private AccountService accountService;
+    @QueryMapping
+    public List<BankAccount> accountsList (){
+        return bankAccountRepository.findAll();
+    }
+    @QueryMapping
+    public BankAccount accountById (@Argument  String id){
+        return bankAccountRepository.findById(id)
+                .orElseThrow(()->new RuntimeException(String.format("Account %s not found",id)));
+    }
+    @MutationMapping
+    public BankAccountResponseDTO addAccount(@Argument BankAccountRequestDTO bankAccount){
+        return accountService.addAcount(bankAccount);
+    }
+}
+/*record BankAccountDTO(Double balance, String type,String currency){
+}*/
